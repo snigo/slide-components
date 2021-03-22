2f8e0008435749cd22a0a7048e298fdcb5bf21ca
snigo <snigo.ogins@gmail.com>
2021-03-22 13:06:52 +0100

    switch from webpack to rollup

---
+++ packages/carousel/rollup.config.js
@@ -0,0 +1,28 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'pkg/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'pkg/index.mjs',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss(),
+  ],
+};
