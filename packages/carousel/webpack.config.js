7145da4abc829e4d27099f2d70494b8379cbced8
snigo <snigo.ogins@gmail.com>
2021-03-22 01:54:57 +0100

    add library target

--- packages/carousel/webpack.config.js
+++ packages/carousel/webpack.config.js
@@ -26,6 +26,8 @@ module.exports = {
   output: {
     filename: 'carousel.js',
     path: path.resolve(__dirname, 'pkg'),
+    library: '',
+    libraryTarget: 'commonjs',
   },
   externals: {
     react: {
